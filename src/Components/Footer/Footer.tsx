4b0005db091f3891ba1cde35c3e97ed4bbc9efa8
Dawidx3p <maildawcio@gmail.com>
2022-07-09 22:57:34 +0200

    update 09.07

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -4,11 +4,11 @@ import '../../Styles/Footer.scss';
 export default function Footer(){
     return(
         <footer>
-            <a href="#"><img className='social-media' width={40} height={40} src='../../logo.svg' alt='Facebook'/></a>
+            <a href="#abc"><img className='social-media' width={40} height={40} src='../../logo.svg' alt='Facebook'/></a>
             <div className='social-media'>
-                <a href="#"><img className='social-media' width={25} height={25} src='../../social-media/facebook.svg' alt='Facebook'/></a>
-                <a href="#"><img className='social-media' width={25} height={25} src='../../social-media/instagram.svg' alt='Instagram'/></a>
-                <a href="#"><img className='social-media' width={25} height={25} src='../../social-media/youtube.svg' alt='Youtube'/></a>
+                <a href="#abc"><img className='social-media' width={25} height={25} src='../../social-media/facebook.svg' alt='Facebook'/></a>
+                <a href="#abc"><img className='social-media' width={25} height={25} src='../../social-media/instagram.svg' alt='Instagram'/></a>
+                <a href="#abc"><img className='social-media' width={25} height={25} src='../../social-media/youtube.svg' alt='Youtube'/></a>
             </div>
         </footer>
     )
